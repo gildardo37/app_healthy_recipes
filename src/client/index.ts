9eb4b0d63899cd338508995b66c37b794efd9d9c
Gildardo Diaz <gildardo.d@stakeordie.com>
2022-04-06 22:02:13 -0700

    Add set token

--- src/client/index.ts
+++ src/client/index.ts
@@ -15,12 +15,28 @@ const client = {
     });
     return await res.json();
   },
+  async getMyMeals() {
+    console.log(this.getToken())
+    const res = await fetch(`${API}/my_meals/getAll`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": this.setToken()
+      },
+    });
+    console.log(res)
+    return await res.json();
+  },
   saveToken(token: string): boolean {
     localStorage.setItem(TOKEN_KEY, token);
     return typeof localStorage.getItem(TOKEN_KEY) !== "undefined";
   },
   getToken(): string | null {
     return localStorage.getItem(TOKEN_KEY);
+  },
+  setToken() {
+    const token = this.getToken() === null ? "" :this.getToken();
+    return `Bearer ${token}`
   },
 };
 
